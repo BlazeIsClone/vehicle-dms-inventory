cd20f6356dffa0c2ef7d5ce8912044e13ecc9c8a
BlazeIsClone <sandevabeykoon123@gmail.com>
2026-05-11 22:16:37 +0530

    refactor: migrate use embeded path

--- internal/database/migrate.go
+++ internal/database/migrate.go
@@ -1,14 +1,18 @@
 package database
 
 import (
+	"embed"
 	"fmt"
 	"os"
 
 	"github.com/golang-migrate/migrate/v4"
 	"github.com/golang-migrate/migrate/v4/database/postgres"
-	_ "github.com/golang-migrate/migrate/v4/source/file"
+	"github.com/golang-migrate/migrate/v4/source/iofs"
 	_ "github.com/joho/godotenv/autoload"
 )
+
+//go:embed migrations/*.sql
+var migrationFiles embed.FS
 
 func Migrate() (*migrate.Migrate, error) {
 	db := New()
@@ -20,11 +24,12 @@ func Migrate() (*migrate.Migrate, error) {
 		return nil, fmt.Errorf("create postgres driver: %w", err)
 	}
 
-	m, err := migrate.NewWithDatabaseInstance(
-		"file://internal/database/migrations",
-		os.Getenv("BLUEPRINT_DB_DATABASE"),
-		driver,
-	)
+	src, err := iofs.New(migrationFiles, "migrations")
+	if err != nil {
+		return nil, fmt.Errorf("create migration source: %w", err)
+	}
+
+	m, err := migrate.NewWithInstance("iofs", src, os.Getenv("BLUEPRINT_DB_DATABASE"), driver)
 	if err != nil {
 		return nil, fmt.Errorf("create migrate instance: %w", err)
 	}
